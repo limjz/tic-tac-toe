958f8cae77363ed34874302de4ff2e7f67387532
limjz <145311062+limjz@users.noreply.github.com>
2026-01-11 10:48:08 +0000

    quit function and keep both client and server running now

--- client.c
+++ client.c
@@ -9,6 +9,10 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 
+#define BUFFER_SIZE 1024
+#define SERVER_PORT 8080
+
+
 
 /* Shared memory constants */
 struct Game
@@ -17,19 +21,44 @@ struct Game
     int currentPlayer;
     bool game_active;   
     pthread_mutex_t board_mutex; 
+    int player_count; //number of connected players
 };
 
 struct Game *gameData;
 const char* SHM_NAME = "/game_shm";
 const size_t SHM_SIZE = sizeof(struct Game);
-const int SERVER_PORT = 8080;
-char server_response[1024];
+char server_response[BUFFER_SIZE];
+int server_socket; // define the socket here so all can use
+
+
+void* receive_handler (void * arg) // pthread to handle the respond or message from the server 
+{
+    char buffer[BUFFER_SIZE];
+
+    while (1){
+        memset(buffer, 0, sizeof(buffer)); //refresh the buffer container 
+        int bytes = recv(server_socket, buffer, BUFFER_SIZE, 0);
+
+        if (bytes <= 0)
+        {
+            printf ("Server Disconnected. GAME OVER \n"); 
+            //break; // break the loop 
+
+            exit (0);//terminate the program 
+        }
+
+        //printf ("\r%s\nServer send ", buffer);
+        //fflush (stdout); 
+
+
+    }
+    return NULL;
+}
 
 
 int main()
 {
-    // create socket 
-    int server_socket;
+    // create the predefine socket 
     server_socket = socket (AF_INET, SOCK_STREAM, 0);
 
     //specify an address for the socket 
@@ -45,13 +74,35 @@ int main()
         printf ("Connection to server failed. Exiting.\n");
         exit (1);
     }
-    else 
-    {
-        printf ("Successfully connected to server on port %d\n", SERVER_PORT);
+    
+    printf ("Server Connected !\n"); 
+
+    // create a pthread to handle the respond from the server// works bts
+    pthread_t recv_thread; 
+    pthread_create (&recv_thread, NULL, receive_handler, NULL); 
+    
+    char user_input [BUFFER_SIZE]; 
+    while (1){ 
+
+        // wait for user input
+        if (fgets(user_input, BUFFER_SIZE, stdin) != NULL)
+        {
+            user_input [strcspn (user_input, "\n")] = 0; //get all the string input before enter key
+
+            if (strcmp(user_input, "QUIT") == 0)
+            {
+                break;
+            }
+
+        }
     }
 
+
+
+
+
     //receive data from server
-    recv (server_socket, server_response, sizeof(server_response), 0);
+    //recv (server_socket, server_response, sizeof(server_response), 0);
 
     //print server response 
     //printf ("Server Response: %s\n", server_response);
--- server.c
+++ server.c
@@ -12,6 +12,10 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <string.h>
+#include <signal.h>
+
+#define BUFFER_SIZE 1024
+#define SERVER_PORT 8080
 
 
 /* Shared memory constants */
@@ -21,25 +25,34 @@ struct Game
     int currentPlayer;
     bool game_active;   
     pthread_mutex_t board_mutex; //mutex for synchronizing access to the game board
-    
-    int player_sockets[2]; //sockets for two players
     int player_count; //number of connected players
 };
 
 struct Game *gameData;
 const char* SHM_NAME = "/game_shm";
 const size_t SHM_SIZE = sizeof(struct Game);
-const int SERVER_PORT = 8080;
-char server_message[1024];
-int PLAYER_IDS[2] = {0, 1};
+char server_message[BUFFER_SIZE];
+int PLAYER_IDS[3] = {0, 1, 2};
 
 
 
 /* Thread functions */  
 void* scheduler_thread (void* arg)
 {
-    // scheduler logic here 
-    // player turns, check win conditions, update game state
+    /*
+    scheduler logic here 
+    player turns, check win conditions, update game state
+    */
+
+    printf("Scheduler start working: Monitoring game state... \n");
+
+    while (gameData ->game_active)  
+    {
+        usleep (10000000); // keep looping to keep the server side alive
+    }
+
+    printf ("Scheduler: Game finished !! Exiting Scheduler_Thread. \n");
+
     return NULL;
 }
 
@@ -51,13 +64,53 @@ void* logger_thread (void* arg)
 
 
 
-void* handle_client (void* arg)
+void handle_client (int client_socket, int player_id, int player_count)
 {
     // Handle client logic here 
-    int my_id = *(int*)arg;
-    printf ("Player %d connected. Current Player: %d (socket=%d)\n", my_id + 1, gameData->currentPlayer, gameData->player_sockets[my_id]);
+    printf ("Player %d connected. Current Player: %d\n", player_id + 1, player_count);
     
-    return NULL;
+    char msg_from_client[BUFFER_SIZE]; // message send from client
+    char *msg = "Welcome to the server !!"; 
+    send (client_socket, msg, strlen(msg), 0);
+ 
+    while (1)
+    {
+        memset (msg_from_client, 0, BUFFER_SIZE); //refresh the buffer container 
+        int bytes = recv (client_socket, msg_from_client, BUFFER_SIZE, 0); 
+
+        if (bytes <= 0)
+        {
+            printf("Player %d disconnected. \n", player_id + 1);
+
+            break;
+        }
+
+
+        printf ("Player %d send %s\n", player_id + 1, msg_from_client);
+    }
+    close(client_socket);
+    exit(0);
+
+}
+
+void signal_handler (int signo)
+{
+    //killing the child process, prevent zombie
+    while( waitpid (-1, NULL, WNOHANG) > 0);
+    {
+        //if the child is dead, the parents still ongoing, close the game-> game_active = false
+        if (gameData != NULL && gameData->game_active) 
+        {
+            gameData->game_active = false; 
+            printf ("Player disconnected. Game quit \n");
+
+            //if server shutdown, all process including the child end as well
+            kill(0, SIGTERM);
+            exit(0);
+        }
+        
+    }
+
 }
 
 
@@ -79,7 +132,7 @@ int main ()
     //size of shared memory
     ftruncate (shm_fd, SHM_SIZE);
 
-    //map shared memory 
+    //map shared memory // init gameData
     gameData = (struct Game*)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0); 
     if (gameData == MAP_FAILED) {
         perror("mmap failed");
@@ -91,7 +144,7 @@ int main ()
     gameData->game_active = true;
     gameData->currentPlayer = 1;
 
-    //initialize Mutex 
+    //Mutex initialization -> for fork() and restricted one player only can access the game board at once
     pthread_mutexattr_t attr;
     pthread_mutexattr_init(&attr);
     pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
@@ -106,11 +159,20 @@ int main ()
     //create Server Socket (listen)
     int server_fd;
     server_fd = socket (AF_INET, SOCK_STREAM, 0);
-    //define the server and client address 
+
+    //define the server address
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons (SERVER_PORT);
     server_address.sin_addr.s_addr = INADDR_ANY;
+
+
+    //avoid address already in use error
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        perror ("setsockopt failed. Exiting.\n");
+        exit (1);
+    }
 
     //bind the socket to the specified IP and port
     if(bind (server_fd, (struct sockaddr *) &server_address, sizeof(server_address)) == -1) {
@@ -118,53 +180,74 @@ int main ()
         exit (1);
     }
 
-    listen (server_fd, 5);
+    listen (server_fd, 5); //maximum server can handle 5 client
 
-    pthread_t client_thread[2];
-    while (playerNumber < 2)
+    
+    signal(SIGCHLD, signal_handler); //check if the player still playing onot, if not end the server also
+
+    while (playerNumber < 3)
     {
-        printf ("Waiting for player %d to connect...\n", playerNumber + 1);
-
         //accept client connections (0 = success, -1 = failure)
+        //define the client address
         struct sockaddr_in client_address;
         socklen_t client_len = sizeof(client_address);
         int new_client_fd = accept (server_fd, (struct sockaddr *) &client_address, &client_len);
         
         if (new_client_fd == -1)
         {
+            if (gameData->game_active == false)
+            {
+                printf("Game stopped, player disconnected, server shutdown"); 
+                kill (0, SIGTERM);
+                exit(0);
+            }
+
+
             perror ("Failed to accept client connection. Exiting.\n");
             exit (1);
         }
         
-        gameData->player_sockets[playerNumber] = new_client_fd;
-        gameData->player_count++;
+        //fork() a new process to handle each client
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            perror ("Fork failed. Exiting.\n");
+            exit (1);
+        }
 
-        pthread_create(&client_thread[playerNumber], NULL, handle_client, (void*)&PLAYER_IDS[playerNumber]);
+        else if (pid == 0) 
+        {
+            // ============= CHILD PROCESS ===========================
 
-        printf ("Client %d connected successfully.\n", gameData->player_count);
+            close (server_fd); //close server socket in child process // child only need use client server
+            handle_client (new_client_fd, PLAYER_IDS[playerNumber], playerNumber + 1);
+
+            exit (0);
+        }
+        else 
+        {
+            // ============= PARENT PROCESS =========================
+
+            close (new_client_fd); //close client socket in parent process // parents are the server no need kacau client
+        }
         
+        printf ("Client %d connected successfully.\n", playerNumber + 1);
         playerNumber++;
         
     }
-
-    //send data to client
-    //strcpy(server_message, "Welcome to the Game Server!");
-    //send(new_client_fd, server_message, sizeof(server_message), 0);
-
 
     
     //wait for client threads to finish
     pthread_join(scheduler, NULL);
     pthread_join(logger, NULL);
 
-    pthread_join(client_thread[0], NULL);
-    pthread_join(client_thread[1], NULL);
 
     //cleanup
     close (server_fd);
     munmap(gameData, SHM_SIZE);
     shm_unlink(SHM_NAME);
     
+    kill(0, SIGTERM);
 
     return 0;
 }
